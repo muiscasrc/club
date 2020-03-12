21d5f9a9522f0844bf7eb05e96a13d190d14f4c0
Edwin Marroquin devem <devemloop@gmail.com>
2020-03-12 11:03:35 -0500

    website MuiscasRC's

---
+++ docs/precache-manifest.cc10b43b289701a6070fa093c14065ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984f7966310a64b96543",
+    "url": "/css/app.6972adce.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fc4d4bbef77b7b46559383453a70942f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9fddb9149f025eda7c",
+    "url": "/js/about.d6fd3372.js"
+  },
+  {
+    "revision": "984f7966310a64b96543",
+    "url": "/js/app.126a97e2.js"
+  },
+  {
+    "revision": "eb5c47a42c44bfee4a6f",
+    "url": "/js/chunk-vendors.816d8bd0.js"
+  },
+  {
+    "revision": "dc5102614dc8b15cffb01e4560f41a60",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
