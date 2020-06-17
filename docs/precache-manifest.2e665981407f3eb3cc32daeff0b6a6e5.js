c83b2f3aeef644e42656b84d3779f66fec9af4e5
devemloop <devemloop@gmail.com>
2020-06-17 15:44:26 -0500

    Update fixed

---
+++ docs/precache-manifest.2e665981407f3eb3cc32daeff0b6a6e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a0b7a67959a6bf63bb",
+    "url": "/club/css/app.57cb79c2.css"
+  },
+  {
+    "revision": "2f98970986677ecb7797422c608a1ccc",
+    "url": "/club/icon.png"
+  },
+  {
+    "revision": "aece1acfc53c29b06bc20966c19a7d39",
+    "url": "/club/img/schoolrugby7.aece1acf.jpg"
+  },
+  {
+    "revision": "eef9e33c3eebc13d4257c7370c08e8be",
+    "url": "/club/img/schoolrugbyleague.eef9e33c.jpg"
+  },
+  {
+    "revision": "20a4a4dc1649f2cf45130dc55834d472",
+    "url": "/club/img/schoolrugbyxv.20a4a4dc.jpg"
+  },
+  {
+    "revision": "6d705c58504742f348aabea1e608cc39",
+    "url": "/club/index.html"
+  },
+  {
+    "revision": "71a0b7a67959a6bf63bb",
+    "url": "/club/js/app.9323a87f.js"
+  },
+  {
+    "revision": "51caa9ce1ed4d3853215",
+    "url": "/club/js/chunk-vendors.60600624.js"
+  },
+  {
+    "revision": "99ab94e9b00909c0c496a32f826a4aef",
+    "url": "/club/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/club/robots.txt"
+  }
+]);
